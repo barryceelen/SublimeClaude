5cb16ff8f53a5d2e72f62c7863c1553a35407c66
Barry Ceelen <barry@multipop.org>
2025-01-03 11:03:29 +0100

    Add max_tokens to settings

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
-PLUGIN_NAME = "SublimeClaude"
-SETTINGS_FILE = "SublimeClaude.sublime-settings"
 ANTHROPIC_VERSION = "2023-06-01";
 DEFAULT_MODEL = "claude-3-opus-20240229"
+MAX_TOKENS = 4000
+PLUGIN_NAME = "SublimeClaude"
+SETTINGS_FILE = "SublimeClaude.sublime-settings"
